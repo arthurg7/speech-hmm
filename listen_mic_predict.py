ce24c33b22704a69a0b4adbb16a304da3622b840
arthurg7 <arthur.tambahani@gmail.com>
2021-05-23 10:34:40 +0700

    Add files via upload

---
+++ listen_mic_predict.py
@@ -0,0 +1,6 @@
+from speech_recogn.listen_mic_predict import listen_predict_mic
+
+#turn on and talk to mic
+MODEL_PATH = 'models/fruit_hmm.pkl'
+
+listen_predict_mic(MODEL_PATH, phrase_time_limit=2)
